590d983411d209738fab2935826d87061dbc06b2
7ussain-3li <105246805+7ussain-3li@users.noreply.github.com>
2024-02-05 17:17:37 +0300

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-
+import { Footer } from "../components/footer/footer.jsx";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
+      <Footer />
     </main>
   );
 }
